460581f6a5a58b575d61bf1646738298332493c5
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-02 01:50:38 +0300

    сonnected to the database

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000
+const port = process.env.PORT;
+const uri = process.env.DB_CONN_STRING!;
+
+mongoose.connect(uri).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error(error);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
 app.listen(port, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${port}`);
 });
