18fffc305ad488b02269b76deec3ff48f4184fbe
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-25 22:42:08 +0300

    добавлен навбар

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Route, Routes} from "react-router";
 import {BrowserRouter} from "react-router-dom";
+import Navbar from '../components/Navbar';
 import PhoneAuthPage from '../pages/PhoneAuthPage';
 import CartPage from '../pages/CartPage';
 
 function App() {
 	return (
 		<BrowserRouter>
+			<Navbar/>
 			<Routes>
-				<Route path="/" element={<PhoneAuthPage/>}/>
+				<Route path="/login" element={<PhoneAuthPage/>}/>
 				<Route path="/cart" element={<CartPage/>}/>
 			</Routes>
 		</BrowserRouter>
