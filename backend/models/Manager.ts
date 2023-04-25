e70dd3746565596614511460319d2e323eda5881
Maxim Elushev <maxim.elushev@yandex.ru>
2023-04-25 23:47:42 +0300

    Add manager model

---
+++ backend/models/Manager.ts
@@ -0,0 +1,17 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+const ManagerSchema: Schema = new Schema({
+	name: {type: String, required: true},
+	email: {type: String, required: true, unique: true},
+	password: {type: String, required: true},
+	phone: {type: String, required: true},
+});
+
+export interface IManager extends Document {
+	name: string;
+	email: string;
+	password: string;
+	phone: string;
+}
+
+export default mongoose.model<IManager>("Manager", ManagerSchema, "managers");
