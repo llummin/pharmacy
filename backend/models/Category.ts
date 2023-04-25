45e9fcf28e165ec203f6fe5ab189328118e82faa
Maxim Elushev <maxim.elushev@yandex.ru>
2023-04-25 23:47:21 +0300

    Add category model

---
+++ backend/models/Category.ts
@@ -0,0 +1,15 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+const CategorySchema: Schema = new Schema({
+	name: {type: String, required: true},
+	description: {type: String, required: true},
+	imageUrl: {type: String, required: true},
+});
+
+export interface ICategory extends Document {
+	name: string;
+	description: string;
+	imageUrl: string;
+}
+
+export default mongoose.model<ICategory>("Category", CategorySchema, "categories");
