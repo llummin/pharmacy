246c8c54dbb17fd4b76a9f1ec0c93673f92fe526
Maxim Elushev <maxim.elushev@yandex.ru>
2023-04-25 23:47:00 +0300

    Add administrator model

---
+++ backend/models/Administrator.ts
@@ -0,0 +1,17 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+const AdministratorSchema: Schema = new Schema({
+	name: {type: String, required: true},
+	email: {type: String, required: true, unique: true},
+	password: {type: String, required: true},
+	phone: {type: String, required: true},
+});
+
+export interface IAdministrator extends Document {
+	name: string;
+	email: string;
+	password: string;
+	phone: string;
+}
+
+export default mongoose.model<IAdministrator>("Administrator", AdministratorSchema, "administrators");
