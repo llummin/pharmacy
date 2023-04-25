5b0d9606629ba58f289d5c9b8571aad107d8402a
Maxim Elushev <maxim.elushev@yandex.ru>
2023-04-25 23:48:14 +0300

    Add request model

---
+++ backend/models/Request.ts
@@ -0,0 +1,30 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+const RequestSchema: Schema = new Schema({
+	manager: {
+		type: Schema.Types.ObjectId,
+		ref: "Manager",
+		required: true,
+	},
+	medicine: {
+		type: Schema.Types.ObjectId,
+		ref: "Medicine",
+		required: true,
+	},
+	quantity: {type: Number, required: true},
+	status: {
+		type: String,
+		enum: ["pending", "fulfilled", "cancelled"],
+		required: true,
+		default: "pending",
+	},
+});
+
+export interface IRequest extends Document {
+	manager: mongoose.Types.ObjectId;
+	medicine: mongoose.Types.ObjectId;
+	quantity: number;
+	status: "pending" | "fulfilled" | "cancelled";
+}
+
+export default mongoose.model<IRequest>("MedicineRequest", RequestSchema, "requests");
