451e5c8d66a070198eff4b48206f11d8be068c11
Maxim Elushev <maxim.elushev@yandex.ru>
2023-04-28 13:17:57 +0300

    sample navbar component

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Store from "./store/Store";
 import "../src/assets/input.css"
 import ShoppingCart from "./components/ShoppingCart";
+import Navbar from "./components/Navbar";
 
 interface State {
     store: Store,
@@ -16,7 +17,7 @@ export const Context = createContext<State>({
 
 ReactDOM.render(
     <Context.Provider value={{store}}>
-        <ShoppingCart/>
+        <Navbar/>
     </Context.Provider>,
     document.getElementById('root')
 );
