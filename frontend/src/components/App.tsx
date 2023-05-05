b64021e978937f87e4731d85921b69ebf8d72946
Maxim Elushev <maxim.elushev@yandex.ru>
2023-05-05 10:40:44 +0300

    project change

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import {Route, Routes} from "react-router";
 import {BrowserRouter} from "react-router-dom";
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import PhoneAuthPage from '../pages/PhoneAuthPage';
-import CartPage from '../pages/CartPage';
-import HomePage from '../pages/HomePage';
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Navbar/>
-			<Routes>
-				<Route path="/login" element={<PhoneAuthPage/>}/>
-				<Route path="/cart" element={<CartPage/>}/>
-				<Route path="/" element={<HomePage/>}/>
-			</Routes>
-			<Footer/>
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+
+        </BrowserRouter>
+    );
 }
 
 export default App;
